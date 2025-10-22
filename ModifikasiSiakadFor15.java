36fd971e7f0cbb7fecdcf4c53b0696331b6bc926
ajiramandani111-ux <ajiramandani111@gmail.com>
2025-10-22 22:35:17 +0700

    ModifikasiSiakadFor

--- ModifikasiSiakadFor15.java
+++ ModifikasiSiakadFor15.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ModifikasiSiakadFor15.java {
+public class ModifikasiSiakadFor15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
